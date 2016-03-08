dcefd6896629e8f43679c298770009877b4a6b68
jgreaser <jgreaser@gmail.com>
2016-03-08 15:00:13 -0500

    Updated CSS

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,6 +12,73 @@ angular.module('app.controllers', [])
 	vm.contact = "";
 	vm.category = "";
 
+	vm.isStarted = false;
+	vm.submitted = false;
+
+	vm.titleIsSet = false;
+	vm.categoryIsSet = false;
+	vm.inspirationIsSet = false;
+	vm.problemIsSet = false;
+	vm.solutionIsSet = false;
+	vm.contactIsSet = false;
+
+	vm.titleIsVisible = false;
+	vm.categoryIsVisible = false;
+	vm.inspirationIsVisible = false;
+	vm.problemIsVisible = false;
+	vm.solutionIsVisible = false;
+	vm.contactIsVisible = false;
+
+	vm.isNotStarted = function(){
+		if (vm.isStarted == false)
+			{return true;}
+		else {return false;}
+	}
+
+	vm.getStarted = function(){
+
+		vm.isStarted = true;
+	}
+
+
+
+	vm.startIsReady = function(){
+		if (vm.titleIsSet == false && vm.inspirationIsSet == false &&	vm.problemIsSet == false  &&	vm.solutionIsSet == false && vm.contactIsSet == false && vm.categoryIsSet == false)
+			{return true;}
+	};
+
+	vm.submitIsReady = function(){
+		if (vm.submitted == false && vm.titleIsSet == true && vm.inspirationIsSet == true &&	vm.problemIsSet == true  &&	vm.solutionIsSet == true && vm.contactIsSet == true && vm.categoryIsSet == true)
+			{return true;}
+	};
+
+	vm.submittedIsTrue = function(){
+		
+			if (vm.submitted == true && vm.titleIsSet == true && vm.inspirationIsSet == true &&	vm.problemIsSet == true  &&	vm.solutionIsSet == true && vm.contactIsSet == true && vm.categoryIsSet == true)
+			{return true;}
+	};
+
+	vm.submitIdea = function (){
+		vm.submitted = true;
+		console.log("SUBMITTED");
+		//vm.startOver();
+		vm.logResponse();
+	}
+
+	vm.startOver = function(){
+	
+		console.log("submitted is false now, check");
+
+			vm.title = "";
+	vm.inspiration = "";
+	vm.problem = "";
+	vm.solution = "";
+	vm.contact = "";
+	vm.category = "";
+
+	vm.isStarted = false;
+	vm.submitted = false;
+
 	vm.titleIsSet = false;
 	vm.categoryIsSet = false;
 	vm.inspirationIsSet = false;
@@ -27,16 +94,8 @@ angular.module('app.controllers', [])
 	vm.contactIsVisible = false;
 
 
-	vm.startIsReady = function(){
-		if (vm.titleIsSet == false && vm.inspirationIsSet == false &&	vm.problemIsSet == false  &&	vm.solutionIsSet == false && vm.contactIsSet == false && vm.categoryIsSet == false)
-			{return true;}
-	};
 
-	vm.submitIsReady = function(){
-		if (vm.titleIsSet == true && vm.inspirationIsSet == true &&	vm.problemIsSet == true  &&	vm.solutionIsSet == true && vm.contactIsSet == true && vm.categoryIsSet == true)
-			{return true;}
 	};
-
 	/*vm.isSet = function(param){
 		if (vm.category != param){return true;}
 
